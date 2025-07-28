48583a99239d52931173d6e325a2e8b31bdd0401
Paras <mehtaparas1901@gmail.com>
2025-07-28 14:20:50 +0530

    authroute updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,10 @@ import userAuth from '../middlewares/userAuth.js';
 const authRouter = Router();
 
 //1 Google oAuth (signup or login through google)
-authRouter.get('/googleAuth', passport.authenticate("google", { scope: ["profile", "email"] }))
+authRouter.get('/googleAuth', passport.authenticate("google", { scope: ['profile', 'email'] }))
 
 //2 Google Oauth callback 
-authRouter.get('/googleAuth/callback', passport.authenticate("google", { session: false,  failureRedirect: `${process.env.FRONTENT_URL}` }), callbackHandler)
+authRouter.get('/googleAuth/callback', passport.authenticate("google", { session: false,  failureRedirect: `${process.env.FRONTENT_URL}/landing` }), callbackHandler)
 
 //3 check whether user is authenticated or not using 'userAuth' middleware
 authRouter.get('/me', userAuth, isAuthenticated);
