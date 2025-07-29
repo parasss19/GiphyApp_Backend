68873c7c40c14d108d679e4c7afdda1b37d62efd
Paras <mehtaparas1901@gmail.com>
2025-07-29 12:01:16 +0530

    cors url updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,  //allow cookies to be sent
 }));
 app.use(passport.initialize());
